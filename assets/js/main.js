969359a18e4d732f00c7e117811ee1c0ae662085
Carter Bian <carterbian32@gmail.com>
2018-12-25 18:55:07 -0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,8 +79,8 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Karl Huang.", "a Student at UCLA.", "a Developer at the Daily Bruin.",
-       "a Foodie exploring Los Angeles."],
+      strings: ["Cupertino Cooking.", "a safe learning environemnt.", "a community open to suggestions.",
+       "here to equip students with life skills."],
       typeSpeed: 100,
       loop: true,
     });
